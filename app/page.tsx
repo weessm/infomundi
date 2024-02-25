8dad10133c70d22e0c6da44a5ab5a37c6018241c
Weslei Miranda <wsmiranda11@gmail.com>
2024-02-25 13:43:12 -0300

    style: corrige margem no eixo y e adequa exibição de nomes

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
   const countries = await getCountries();
 
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-4 mt-16">
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-4 my-16">
       {countries.map((country) => (
         <CountryCardComponent
           key={country.name.common}
--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -19,7 +19,7 @@ const CountryCardComponent = ({
         <div className="relative w-full h-60 sm:h-40 overflow-hidden p-2 rounded-xl">
           <Image src={flagSvg} alt={flagAlt} fill className="object-cover" />
         </div>
-        <h1 className="font-bold text-xl text-center mt-3">{name}</h1>
+        <h1 className="font-bold text-lg text-center mt-3">{name}</h1>
       </article>
     </Link>
   );
